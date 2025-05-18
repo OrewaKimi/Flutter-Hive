025c2dd437655591c560d21fa286fc5f29da29ca
OrewaKimi <kimi93.com@gmail.com>
2025-05-18 15:00:45 +0700

    update button to do and dialog

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,44 +9,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-// get the box
-final _myBox = Hive.box('MY_BOX');
+  // get the box
+  final _myBox = Hive.box('MY_BOX');
 
-// text controller
-final _textController = TextEditingController();
+  // text controller
+  final _textController = TextEditingController();
 
-// list of todos
-List todos = [];
+  // list of todos
+  List todos = [];
 
   @override
   void initState() {
-      // load data, if none exist then defalut
-      todos = _myBox.get('TODO_LIST') ?? [];
+    // load data, if none exist then defalut
+    todos = _myBox.get('TODO_LIST') ?? [];
 
     super.initState();
   }
 
   // open new todo dialog
-  void openNewTodo(){
+  void openNewTodo() {
     showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: const Text('Add Task'),
-      content: TextField(
-        controller: _textController,
-      ),
-    ),
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: const Text('Add Task'),
+            content: TextField(controller: _textController),
+            actions: [
+              // cancel button
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _textController.clear();
+                },
+                child: const Text('Cancel'),
+              ),
+
+              // add button
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  addTodo();
+                },
+                child: const Text('Add'),
+              ),
+            ],
+          ),
     );
   }
 
-
   // add new todo
+  void addTodo() {
+    // add todo to list
+    setState(() {
+      todos.add(_textController.text);
+      _textController.clear();
+    });
+  }
 
   // delete todo
 
-
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
+
+      // add button
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
