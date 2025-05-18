a92ccd342b085e401350aa957d95ee97be5aeb14
OrewaKimi <kimi93.com@gmail.com>
2025-05-18 15:36:23 +0700

    update ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_ce/hive.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,64 +10,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // get the box
   final _myBox = Hive.box('MY_BOX');
-
-  // text controller
   final _textController = TextEditingController();
-
-  // list of todos
   List todos = [];
 
   @override
   void initState() {
-    // load data, if none exist then defalut
     todos = _myBox.get('TODO_LIST') ?? [];
-
     super.initState();
   }
 
-  // open new todo dialog
   void openNewTodo() {
     showDialog(
       context: context,
-      builder:
-          (context) => AlertDialog(
-            title: const Text('Add Task'),
-            content: TextField(controller: _textController),
-            actions: [
-              // cancel button
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  _textController.clear();
-                },
-                child: const Text('Cancel'),
-              ),
-
-              // add button
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  addTodo();
-                },
-                child: const Text('Add'),
-              ),
-            ],
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        title: Text('Add Task', style: GoogleFonts.poppins(fontWeight: FontWeight.bold)),
+        content: TextField(
+          controller: _textController,
+          autofocus: true,
+          decoration: InputDecoration(
+            hintText: 'Enter your task...',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
           ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              _textController.clear();
+            },
+            child: Text('Cancel', style: GoogleFonts.poppins(color: Colors.grey[600])),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+              addTodo();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurpleAccent,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            ),
+            child: Text('Add', style: GoogleFonts.poppins(color: Colors.white)),
+          ),
+        ],
+      ),
     );
   }
 
-  // add new todo
   void addTodo() {
-    // add todo to list
+    String todo = _textController.text;
     setState(() {
-      todos.add(_textController.text);
+      todos.add(todo);
       _textController.clear();
     });
+    saveToDatabase();
   }
 
-  // delete todo
   void deleteTodo(int index) {
     setState(() {
       todos.removeAt(index);
@@ -74,38 +79,71 @@ class _HomePageState extends State<HomePage> {
     saveToDatabase();
   }
 
-
-  // save to database
   void saveToDatabase() {
     _myBox.put('TODO_LIST', todos);
   }
 
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      // add button
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('My To-Do List', style: GoogleFonts.poppins(color: Colors.black)),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+        onPressed: openNewTodo,
+        backgroundColor: Colors.deepPurpleAccent,
+        child: const Icon(Icons.add, color: Colors.white),
       ),
-
-      // display List of todos
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          // get each todo
-          final todo = todos[index];
-
-          return ListTile(
-            title: Text(todo),
-            trailing: IconButton(
-               onPressed: () => deleteTodo(index),
-              icon: const Icon(Icons.delete),
+      body: todos.isEmpty
+          ? Center(
+              child: Text(
+                'No tasks yet!',
+                style: GoogleFonts.poppins(fontSize: 16, color: Colors.grey),
+              ),
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                final todo = todos[index];
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 12),
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 8,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      const Icon(Icons.check_circle_outline, color: Colors.deepPurpleAccent),
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: Text(
+                          todo,
+                          style: GoogleFonts.poppins(fontSize: 16),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => deleteTodo(index),
+                        icon: const Icon(Icons.delete_outline, color: Colors.redAccent),
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          ); 
-        },
-      ),
     );
   }
 }
