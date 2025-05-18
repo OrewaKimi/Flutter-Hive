3340ca37d251d4d6d86e456714bdb06380afb1eb
OrewaKimi <kimi93.com@gmail.com>
2025-05-18 14:41:18 +0700

    box and list todos

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce/hive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +9,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+// get the box
+final _myBox = Hive.box('MY_BOX');
+
+// text controller
+final _textController = TextEditingController();
+
+// list of todos
+List todos = [];
+
   @override
   Widget build(BuildContext context) {
+    // load data, if none exist then defalut
+    super.initState();
+    
     return Scaffold(
-      
+
     );
   }
 }
