ba9d4dc47c371b3085fa05b40e000715135b54f3
OrewaKimi <kimi93.com@gmail.com>
2025-05-18 14:36:55 +0700

    initialize hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hive/home_page.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 
-void main() {
+void main()  async {
+// Initialize Hive
+await Hive.initFlutter();
+
+// Open a box
+await Hive.openBox('MY_BOX');
+
   runApp(const MyApp());
 }
 
